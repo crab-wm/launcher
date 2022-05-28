8e65eb167cfa4b8afb56ac4d32663be08ebd2b55
PiotrekPKP <peterpkppeter@gmail.com>
2022-05-28 08:16:37 +0200

    feat: Check if process is running

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ use std::io::Write;
 use std::process::exit;
 use std::rc::Rc;
 use gtk::glib::{clone, MainContext, MainLoop, PRIORITY_DEFAULT, Receiver};
+use sysinfo::{System, SystemExt};
 
 use crate::config::Config;
 use crate::utils::{display_err};
@@ -29,10 +30,15 @@ use window::Window;
 
 #[tokio::main]
 async fn main() {
-    gio::resources_register_include!("crab-launcher.gresource").expect(ERROR_RESOURCES);
-
     let mut args = std::env::args();
     let arg = args.nth(1);
+
+    let s = System::new_all();
+    let is_running = s.processes_by_exact_name(APP_TITLE).count() > 1;
+
+    if is_running {
+        emit_show_window();
+    }
 
     if let Some(arg) = arg {
         match arg.as_str() {
@@ -49,39 +55,39 @@ async fn main() {
                 println!("{}", CONFIG_GENERATED);
                 exit(0);
             }
-            "--daemon" => {
-                let crab_daemon = CrabDaemonServer::new();
-                let (tx, rx) = MainContext::channel::<bool>(PRIORITY_DEFAULT);
-                let rx = Rc::new(RefCell::new(Some(rx)));
+            "--show" => {
+                gio::resources_register_include!("crab-launcher.gresource").expect(ERROR_RESOURCES);
 
                 let app = Application::builder().application_id(APP_ID).build();
 
                 app.connect_startup(|_| load_css());
-                app.connect_activate(move |app| build_ui(app, false, Some(rx.clone())));
-
-                let owner_id = crab_daemon.start(tx);
+                app.connect_activate(|app| build_ui(app, true, None));
 
                 app.run_with_args::<&str>(&[]);
-
-                MainLoop::new(None, false).run();
-                gio::bus_unown_name(owner_id);
             },
-            "--show" => {
-                let crab_daemon = CrabDaemonClient::new();
-                crab_daemon.run_method("ShowWindow");
-            }
             a => display_err(format!("Uknown parameter: {}", a).as_str()),
         }
 
         return;
     }
 
+    gio::resources_register_include!("crab-launcher.gresource").expect(ERROR_RESOURCES);
+
+    let crab_daemon = CrabDaemonServer::new();
+    let (tx, rx) = MainContext::channel::<bool>(PRIORITY_DEFAULT);
+    let rx = Rc::new(RefCell::new(Some(rx)));
+
     let app = Application::builder().application_id(APP_ID).build();
 
     app.connect_startup(|_| load_css());
-    app.connect_activate(|app| build_ui(app, true, None));
+    app.connect_activate(move |app| build_ui(app, false, Some(rx.clone())));
+
+    let owner_id = crab_daemon.start(tx);
 
     app.run();
+
+    MainLoop::new(None, false).run();
+    gio::bus_unown_name(owner_id);
 }
 
 fn load_css() {
@@ -117,4 +123,9 @@ fn build_ui(app: &Application, show_window: bool, rx: Option<Rc<RefCell<Option<R
     if show_window {
         window.present();
     }
+}
+
+fn emit_show_window() {
+    let crab_daemon = CrabDaemonClient::new();
+    crab_daemon.run_method("ShowWindow");
 }
